91722e4b6eee62da22122b6e008cc3e28d2115c3
joysen <joysenr96@gmail.com>
2022-09-20 15:52:14 +0600

    yes

--- Configs/DB.js
+++ Configs/DB.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const ConnectDB = async () => {
   try {
     const DB_URL = process.env.MDB_URL;
-    const conn = await mongoose.connect(DB_URL, {
+    const url =
+      "mongodb+srv://my-Tours:JQxqcVKGkFWx9jJu@cluster0.gcuiy.mongodb.net/Tours-app?retryWrites=true&w=majority";
+    const conn = await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
